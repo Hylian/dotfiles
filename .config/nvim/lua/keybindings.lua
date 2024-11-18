dbffbe5868b28bfdb1e0832fe7d0246067a65296
Edward Shin <contact@edwardsh.in>
2024-11-18 15:56:21 -0500

    update nvim bindings

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -6,8 +6,8 @@ local function map(mode, combo, mapping, opts)
   vim.api.nvim_set_keymap(mode, combo, mapping, options)
 end
 
-vim.g.mapleader = ',' -- change the <leader> key to be comma
-vim.g.maplocalleader = ",,"
+vim.g.mapleader = ';' -- change the <leader> key to be comma
+vim.g.maplocalleader = ";;"
 
 map('n', '<CR>',      ":noh<CR><CR>", {noremap = true})
 map('n', 'gd',        "<cmd>lua require('fzf-lua').lsp_definitions()<CR>")
@@ -15,12 +15,11 @@ map('n', 'gr',        "<cmd>lua require('fzf-lua').lsp_references()<CR>")
 map('n', ']',         "<cmd>lua require('fzf-lua').lsp_finder()<CR>")
 map('n', '{',         "<cmd>lua require('fzf-lua').lsp_live_workspace_symbols()<CR>")
 map('n', '}',         "<cmd>lua require('fzf-lua').lsp_document_symbols()<CR>")
-map('n', '\'',        "<cmd>lua require('fzf-lua').oldfiles()<CR>")
-map('n', '\"',        "<cmd>lua require('fzf-lua').files()<CR>")
-map('n', '_',         "<cmd>lua require('fzf-lua').grep_project()<CR>")
-map('n', '<C-_>',     "<cmd>lua require('fzf-lua').grep_last()<CR>")
-map('n', '|',         "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>")
-map('n', '<Leader>s', "<cmd>lua require('fzf-lua').grep_cword()<CR>")
+map('n', '|',        "<cmd>lua require('fzf-lua').oldfiles()<CR>")
+map('n', '_',        "<cmd>lua require('fzf-lua').files()<CR>")
+map('n', '\'',        "<cmd>lua require('fzf-lua').grep_curbuf()<CR>")
+map('n', '\"',        "<cmd>lua require('fzf-lua').grep_project()<CR>")
+map('n', '<C-\'>',    "<cmd>lua require('fzf-lua').grep_cword()<CR>")
 map('n', '[',         "<cmd>:ClangdSwitchSourceHeader<CR>")
 map('n', '<TAB>',     "<cmd>:ToggleDiag<CR>")
 map('n', '<silent>',  "gb :BufferLinePick<CR>")
@@ -33,9 +32,15 @@ map('n', '<C-A-6>',   "<Cmd>BufferLineGoToBuffer 6<CR>")
 map('n', '<C-A-7>',   "<Cmd>BufferLineGoToBuffer 7<CR>")
 map('n', '<C-A-8>',   "<Cmd>BufferLineGoToBuffer 8<CR>")
 map('n', '<C-A-9>',   "<Cmd>BufferLineGoToBuffer 9<CR>")
+map('n', '.',   "ms*")
+map('n', ',',   "ms#")
+map('n', '<Esc>', ":noh<CR>")
 
 map('n', '<',      "<<", {noremap = true})
 map('n', '>',      ">>", {noremap = true})
+
+map('v', '<',      "<gv", {noremap = true})
+map('v', '>',      ">gv", {noremap = true})
 
 map('i', '',  "<C-W>")
 
