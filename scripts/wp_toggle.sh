c7cc53be8076fa0f20f7e367fa33da6d46c12694
Edward Shin <contact@edwardsh.in>
2024-12-04 14:24:28 -0500

    update wp_toggle for kanto

--- scripts/wp_toggle.sh
+++ scripts/wp_toggle.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-headphones_id=$(pw-cli info alsa_output.usb-Topping_D50-00.HiFi__Headphones__sink | head -n 1 | awk '{print $2}')
-speakers_id=$(pw-cli info alsa_output.usb-Focusrite_Scarlett_Solo_USB_Y7WBXER4518154-00.HiFi__Line1__sink | head -n 1 | awk '{print $2}')
+headphones_name="alsa_output.usb-Topping_D50-00.HiFi__Headphones__sink"
+speakers_name="alsa_output.usb-Burr-Brown_from_TI_USB_Audio_DAC-00.analog-stereo"
 
-if wpctl status | /usr/bin/rg -A 2 Settings | /usr/bin/rg -q Scarlett; then
+headphones_id=$(pw-cli info $headphones_name | head -n 1 | awk '{print $2}')
+speakers_id=$(pw-cli info $speakers_name | head -n 1 | awk '{print $2}')
+
+if wpctl status | /usr/bin/rg -A 2 Settings | /usr/bin/rg -q $speakers_name; then
   wpctl set-default "$headphones_id"
 else
   wpctl set-default "$speakers_id"
