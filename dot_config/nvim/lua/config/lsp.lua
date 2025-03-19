b8327f46d4501d48b13554677d254bc51e47e95d
Edward Shin <shined@google.com>
2025-03-19 15:56:37 -0400

    work desktop initial config

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -1,8 +1,11 @@
 require('toggle_lsp_diagnostics').init({ start_on = false })
 
 require('mason').setup({
+  PATH = "append"
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig').clangd.setup{}
+require('lspconfig').clangd.setup{
+  cmd = {"clangd", "--header-insertion=never"}
+}
 require('lspconfig').rust_analyzer.setup{}
