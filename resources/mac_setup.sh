000529276a7b25c872be8fe99ae80dd9cd47eeb5
Edward Shin <contact@edwardsh.in>
2025-05-11 18:30:43 -0400

    fbkbook reconcile

---
+++ resources/mac_setup.sh
@@ -0,0 +1,4 @@
+# Drag windows with control + cmd
+defaults write -g NSWindowShouldDragOnGesture -bool true
+# Don't play animation when dragging
+defaults write -g NSWindowShouldDragOnGestureFeedback -bool false
