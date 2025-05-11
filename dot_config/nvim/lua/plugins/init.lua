614b9c5da7b346b170fd426c019aec6ec2fd6417
Edward Shin <contact@edwardsh.in>
2025-05-11 17:01:10 -0400

    mbp 16 install

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
+  { 'junegunn/fzf.vim' },
   { 'ibhagwan/fzf-lua', branch='main' },
   { 'tpope/vim-commentary' },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
@@ -113,8 +114,9 @@ return {
   },
   { 'nvim-focus/focus.nvim', version = false },
   {'akinsho/toggleterm.nvim', version = "*", config = true},
+  --{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
   { 'echasnovski/mini.nvim', version = '*' },
   {
     'MagicDuck/grug-far.nvim',
