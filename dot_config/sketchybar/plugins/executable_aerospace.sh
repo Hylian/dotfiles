5879b3cdfaed82e11950878f13e118c4914f0b91
Edward Shin <contact@edwardsh.in>
2025-05-12 12:11:36 -0400

    fix sketchybar concurrency issues

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -1,4 +1,6 @@
-#!/bin/dash
+#!/usr/bin/env bash
+
+# Requires brew bash and flock
 
 TEXT_ACTIVE=0xffd3c6aa
 TEXT_INACTIVE=0x8fd3c6aa
@@ -7,6 +9,10 @@ BG_ACTIVE=0xa03c4841
 BG_INACTIVE=0x603c4841
 BG_INACTIVE_EMPTY=0x303c4841
 
+lockfile="/tmp/sketchybar_lock"
+exec {fd}>$lockfile
+flock --timeout 2 "$fd" || exit 1
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME label.color="$TEXT_ACTIVE" background.color="$BG_ACTIVE"
 elif [ $(aerospace list-windows --workspace "$1" --count) -gt 0 ]; then
@@ -14,3 +20,5 @@ elif [ $(aerospace list-windows --workspace "$1" --count) -gt 0 ]; then
 else
     sketchybar --set $NAME label.color="$TEXT_INACTIVE_EMPTY" background.color="$BG_INACTIVE_EMPTY"
 fi
+
+exit 0
