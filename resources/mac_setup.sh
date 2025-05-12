b882a47e5cf1eebbff9e6f0fb17aeee0a684b858
Edward Shin <contact@edwardsh.in>
2025-05-12 02:15:51 -0400

    update mac setup

--- resources/mac_setup.sh
+++ resources/mac_setup.sh
@@ -2,3 +2,5 @@
 defaults write -g NSWindowShouldDragOnGesture -bool true
 # Don't play animation when dragging
 defaults write -g NSWindowShouldDragOnGestureFeedback -bool false
+# Disable window opening animations
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
