7971a002d488395b147ddcebd9c1d1d640576b9b
Edward Shin <contact@edwardsh.in>
2025-05-14 13:50:33 -0400

    add raycast to brew script

--- resources/mac_brew.sh
+++ resources/mac_brew.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask font-hack-nerd-font
+brew install --cask raycast
 brew install neovim chezmoi sketchybar antigen fzf zoxide ripgrep starship direnv cmake ccache fzf bat ripgrep eza borders fd flock util-linux
